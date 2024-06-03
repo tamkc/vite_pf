6249d59d0ffc6e0e0a07d58e26ca3f42388ad295
ptamhunghing <tamkc1999@gmail.com>
2024-06-03 16:52:27 +0800

    [FEAT] Added Work Selection

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import Sidenav from './components/Sidenav'
-import Main from './components/Main'
-import Work from './components/Work'
+import { useState } from "react";
+import Sidenav from "./components/Sidenav";
+import Main from "./components/Main";
+import Work from "./components/Work";
+import Projects from "./components/Projects";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       <Sidenav />
       <Main />
       <Work />
+      <Projects />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ portfolio/src/components/Projects.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ProjectItem from "./Projectitem";
+import propertyImg from "../assets/odoo_payroll.png";
+import property2Img from "../assets/payroll_system.png";
+import property3Img from "../assets/react.svg";
+import property4Img from "../assets/react.svg";
+
+const Projects = () => {
+  return (
+    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 py-16 p-4">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">Project</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, aperiam
+        iusto! Nam doloremque inventore esse perferendis ducimus beatae nobis at
+        alias tempora cupiditate officiis, iste blanditiis voluptates animi
+        consequatur quis?
+      </p>
+
+      <div className="grid grid-cols-2 md:grid-cols-2 sm:grid-cols-2 gap-12">
+        <ProjectItem
+          img={propertyImg}
+          title="Payroll Backend"
+          stack="Python, Odoo"
+        />
+        <ProjectItem
+          img={property2Img}
+          title="Payroll Frontend"
+          stack="React, NextJs, NodeJs"
+        />
+        <ProjectItem img={property3Img} title="Project 3" />
+        <ProjectItem img={property4Img} title="Project 4" />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
